ee672af6648f62a6f733e5bae8832a5602db0640
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-09-02 18:07:52 -0300

    Chore
    Configuração e criação do projeto JavaFx

---
+++ sgtg/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = FXMLLoader.load(getClass().getResource("/gui/Main.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
