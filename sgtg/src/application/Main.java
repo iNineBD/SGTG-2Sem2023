3a8268f34416c265b8d3946aab8e7ee3c3e54160
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-09-17 21:48:05 -0300

    feat
    inserção do menuBar e tela "Sobre"

--- sgtg/src/application/Main.java
+++ sgtg/src/application/Main.java
@@ -1,26 +1,52 @@
 package application;
-	
+
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("/gui/Main.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
+			
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/Main.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			this.mainScene = new Scene(scrollPane);
+			
+			primaryStage.setScene(mainScene);
+			
+			
+			Image iconeInine = new Image(getClass().getResourceAsStream("/imgs/inine_logo.png"));
+			
+			primaryStage.getIcons().add(iconeInine);
+			primaryStage.setTitle("Sistema Gerenciador de TG's");
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
