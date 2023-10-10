5fce0a92746abf3989b2146485936e139b84f247
Andre-Bernardes200 <127262908+Andre-Bernardes200@users.noreply.github.com>
2023-10-10 20:53:17 -0300

    fix: Orientador não fica repetindo no banco de dados.

--- sgtg/src/gui/util/Telas.java
+++ sgtg/src/gui/util/Telas.java
@@ -6,14 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.Year;
 
 import application.Main;
 import conexao.DB;
 import gui.TelaGerenciarAlunosController;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import entidades.Aluno;
 import gui.TelaConfirmaController;
@@ -284,13 +282,19 @@ public class Telas {
     	
     	stMatricula = conecta.prepareStatement("insert into matricula(id_aluno,id_turma) values(?,?)");
     	
+    	//Procurar se já existe o orientador no banco
+    	stBuscaEmailOrientador.setString(1, aluno.getEmailFatecOrientador());
+    	ResultSet result1 = stBuscaEmailOrientador.executeQuery();
+    	
+    	if(!result1.next()){
+    	
     	stOrientador.setString(1,aluno.getOrientador());
     	stOrientador.setString(2, aluno.getEmailFatecOrientador());
     	stOrientador.executeUpdate();
     	
     	// Para fazer a consulta e achar o ID do orientador
-    	stBuscaEmailOrientador.setString(1, "%" + aluno.getEmailFatecOrientador() + "%" );
-    	ResultSet result1 = stBuscaEmailOrientador.executeQuery();
+    	stBuscaEmailOrientador.setString(1, aluno.getEmailFatecOrientador());
+    	result1 = stBuscaEmailOrientador.executeQuery();
     	result1.next();
     	int idOrientador = result1.getInt("id");
     	
@@ -350,7 +354,7 @@ public class Telas {
         	stTg.setInt(5, idTipo);
         	stTg.executeUpdate();
     	}catch(SQLException e) {
-//    		e.printStackTrace();
+    		e.printStackTrace();
     		stAnoESemestre.setInt(1,semestre);
     		stAnoESemestre.setInt(2, ano);
     		stAnoESemestre.executeUpdate();
@@ -417,14 +421,142 @@ public class Telas {
     	stTg.setInt(5, idTipo);
     	stTg.executeUpdate();
     	}
+    	}else {
+        	// Para fazer a consulta e achar o ID do orientador
+        	int idOrientador = result1.getInt("id");
+        	
+        	stAluno.setString(1, aluno.getNome());
+        	stAluno.setString(2, aluno.getEmailFatecAluno());
+        	stAluno.setString(3, aluno.getEmailPessoal());
+        	stAluno.setInt(4, idOrientador);
+        	stAluno.executeUpdate();
+        	
+        	// Para fazer a consulta e localizar o ID do aluno
+        	stBuscaIdAluno.setString(1, aluno.getEmailFatecAluno());
+        	ResultSet result3 = stBuscaIdAluno.executeQuery();
+        	result3.next();
+        	int idAluno = result3.getInt("id");
+        	
+        	// Para fazer a consulta e localizar o ID da turma
+        	stBuscaIdTurma.setString(1, aluno.getNomeTurma());
+        	ResultSet result2 = stBuscaIdTurma.executeQuery();
+            	
+        	if(!result2.next()) {
+        	// Verificação se no banco já existe ano e semestre
+        	try {
+        		stBuscaSemestreEAno.setInt(1, semestre);
+        		stBuscaSemestreEAno.setInt(2,ano);
+        		ResultSet result5 = stBuscaSemestreEAno.executeQuery();
+        		result5.next();
+        		int sem = result5.getInt("semestralizacao");
+        		int year = result5.getInt("ano");
+            	stTurma.setString(1,aluno.getNomeTurma());
+            	stTurma.setInt(2, sem);
+            	stTurma.setInt(3, year);
+            	stTurma.executeUpdate();
+            	
+            	stBuscaIdTurma.setString(1, aluno.getNomeTurma());
+            	result2 = stBuscaIdTurma.executeQuery();
+            	result2.next();
+            	int idTurma = result2.getInt("id");
+            	
+            	stMatricula.setInt(1, idAluno);
+            	stMatricula.setInt(2, idTurma);
+            	stMatricula.executeUpdate();
+            	       	
+            	stTipo.setString(1, aluno.getTipoTG());
+            	stTipo.setString(2, "123");
+            	stTipo.executeUpdate();
+            	
+            	// Para fazer a consulta e localizar o ID do tipo
+            	stBuscaIdTipo.setString(1, aluno.getTipoTG());
+            	ResultSet result4 = stBuscaIdTurma.executeQuery();
+            	result4.next();
+            	int idTipo = result4.getInt("id");
+            	
+            	stTg.setString(1, aluno.getProblemaResolvidoOuEstudoArtigo());
+            	stTg.setString(2, aluno.getEmpresa());
+            	stTg.setString(3, aluno.getDisciplina());
+            	stTg.setInt(4, idAluno);
+            	stTg.setInt(5, idTipo);
+            	stTg.executeUpdate();
+        	}catch(SQLException e) {
+        		e.printStackTrace();
+        		stAnoESemestre.setInt(1,semestre);
+        		stAnoESemestre.setInt(2, ano);
+        		stAnoESemestre.executeUpdate();
+        		
+        		stBuscaSemestreEAno.setInt(1, semestre);
+        		stBuscaSemestreEAno.setInt(2,ano);
+        		ResultSet result5 = stBuscaSemestreEAno.executeQuery();
+        		result5.next();
+        		int sem = result5.getInt("semestralizacao");
+        		int year = result5.getInt("ano");
+            	stTurma.setString(1,aluno.getNomeTurma());
+            	stTurma.setInt(2, sem);
+            	stTurma.setInt(3,year);
+            	stTurma.executeUpdate();
+            	
+            	stBuscaIdTurma.setString(1, aluno.getNomeTurma());
+            	result2 = stBuscaIdTurma.executeQuery();
+            	result2.next();
+            	int idTurma = result2.getInt("id");
+            	stMatricula.setInt(1, idAluno);
+            	stMatricula.setInt(2, idTurma);
+            	stMatricula.executeUpdate();
+            	
+            	
+            	stTipo.setString(1, aluno.getTipoTG());
+            	stTipo.setString(2, "123");
+            	stTipo.executeUpdate();
+            	
+            	// Para fazer a consulta e localizar o ID do tipo
+            	stBuscaIdTipo.setString(1, aluno.getTipoTG());
+            	ResultSet result4 = stBuscaIdTurma.executeQuery();
+            	result4.next();
+            	int idTipo = result4.getInt("id");
+            	
+            	stTg.setString(1, aluno.getProblemaResolvidoOuEstudoArtigo());
+            	stTg.setString(2, aluno.getEmpresa());
+            	stTg.setString(3, aluno.getDisciplina());
+            	stTg.setInt(4, idAluno);
+            	stTg.setInt(5, idTipo);
+            	stTg.executeUpdate();
+        	}}
+        	else {
+        	int idTurma = result2.getInt("id");
+        	
+        	stMatricula.setInt(1, idAluno);
+        	stMatricula.setInt(2, idTurma);
+        	stMatricula.executeUpdate();
+        	
+        	
+        	stTipo.setString(1, aluno.getTipoTG());
+        	stTipo.setString(2, "123");
+        	stTipo.executeUpdate();
+        	
+        	// Para fazer a consulta e localizar o ID do tipo
+        	stBuscaIdTipo.setString(1, aluno.getTipoTG());
+        	ResultSet result4 = stBuscaIdTurma.executeQuery();
+        	result4.next();
+        	int idTipo = result4.getInt("id");
+        	
+        	stTg.setString(1, aluno.getProblemaResolvidoOuEstudoArtigo());
+        	stTg.setString(2, aluno.getEmpresa());
+        	stTg.setString(3, aluno.getDisciplina());
+        	stTg.setInt(4, idAluno);
+        	stTg.setInt(5, idTipo);
+        	stTg.executeUpdate();
+        	}
+    		
+    	}
 
     	} catch (SQLException e) {
 			// TODO Auto-generated catch block
     		e.printStackTrace();
 //    		Alerts.showAlert("SQL Exception","Erro","Este aluno já está cadastrado", AlertType.ERROR);
 		} 
-    	
-    	
+    		
     	}
 	
 	
