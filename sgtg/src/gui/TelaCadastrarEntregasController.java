59940eec7ed5f9cc3d0821a57f3eb7f2934ffcc9
João Lamão <joaomatheuslamao9@gmail.com>
2023-11-12 01:05:02 -0300

    fix: cadastro de entregas parcial
    falta inserir relacoes nas tabelas associativas

--- sgtg/src/gui/TelaCadastrarEntregasController.java
+++ sgtg/src/gui/TelaCadastrarEntregasController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import com.mysql.cj.protocol.Resultset;
+
 import conexao.DB;
-import dto.TurmasDTO;
+import dto.TipoDTO;
 import gui.util.Alerts;
-import gui.util.LoadTurmas;
+import gui.util.LoadTipo;
 import gui.util.Telas;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,6 +24,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -33,7 +37,12 @@ public class TelaCadastrarEntregasController implements Initializable {
 	private Label lbSemestreAno;
 
 	@FXML
-	private ChoiceBox<TurmasDTO> choiceBoxTurma;
+	private ChoiceBox<TipoDTO> choiceBoxTipo = new ChoiceBox<TipoDTO>();
+	
+	@FXML
+	private CheckBox check1;
+	@FXML
+	private CheckBox check2;
 
 	@FXML
 	private TextField txtFieldTituloEntrega;
@@ -52,74 +61,38 @@ public class TelaCadastrarEntregasController implements Initializable {
 
 	public void onBtnSalvarAction() throws SQLException {
 
-		TurmasDTO turma_selecionada = choiceBoxTurma.getValue();
+		TipoDTO tipo = choiceBoxTipo.getValue();
 		String titulo = txtFieldTituloEntrega.getText();
 		String descricao = txtAreaDescricao.getText();
 		LocalDate data = datePickerDataFinal.getValue();
-		List<String> nome_entrega = new ArrayList<String>();
-
-		if (turma_selecionada == null || titulo.trim().isEmpty() || descricao.trim().isEmpty() || data == null) {
+		
+		if (tipo == null || titulo.trim().isEmpty() || descricao.trim().isEmpty() || data == null) {
 			Alerts.showAlert("Campo nulo", "Cuidado", "Todos os campos devem ser preenchidos", AlertType.WARNING);
 		} else {
-			LocalDate dataAtual = LocalDate.now();
-
-			// Verifica se localData é menor do que a data atual
-			if (data.isBefore(dataAtual)) {
-				Alerts.showAlert("Data Anterior a Atual", "Atenção", "A data selecionada é anterior a data atual", AlertType.WARNING);
-			} else {
-				new DB();
-				Connection conn = DB.getConnection();
-
-				PreparedStatement st2 = conn.prepareStatement("select titulo_entrega from entrega");
-				
-				ResultSet result = st2.executeQuery();
-
-				while (result.next()) {
-					nome_entrega.add(result.getString("titulo_entrega"));
-				}
-				boolean nome_repetido = false;
-				for (String nome : nome_entrega) {
-					if (nome.equals(titulo)) {
-						nome_repetido = true;
-						break;
-					}
-
-				}
-				if (nome_repetido) {
-					Alerts.showAlert("Titulo de entrega", "Titulo de entrega ja cadastrado",
-							"O titulo inserido já foi ultilizado", AlertType.WARNING);
-				} else {
-					PreparedStatement st = conn.prepareStatement(
-							"insert into entrega (titulo_entrega, data_entrega, descricao, id_turma) values (?, ?, ?, ?)");
-					st.setString(1, titulo);
-					st.setObject(2, data);
-					st.setString(3, descricao);
-					st.setInt(4, turma_selecionada.getId());
-
-					st.executeUpdate();
-					
-					Alerts.showAlert("Sucesso", "Entrega cadastrada!!!",
-							"A entrega foi cadastrada com sucesso", AlertType.INFORMATION);
-					
-					txtAreaDescricao.setText("");
-					
-					txtFieldTituloEntrega.setText("");
-					
-					datePickerDataFinal.setValue(null);
-					
-					choiceBoxTurma.setValue(null);
-					
-				}
-
+			DB db = new DB();
+			Connection con = db.getConnection();
+			CallableStatement cs = con.prepareCall("{CALL pr_insert_entrega(?, ?, ?)}");
+			
+			cs.setString(1, titulo);
+			cs.setObject(2, data);
+			cs.setString(3, descricao);
+			ResultSet result = cs.executeQuery();
+			while (result.next()) {
+				int id_entrega = result.getInt("id");
+				System.out.println(id_entrega);
 			}
-
+			
+			cs.close();
+			
+			
 		}
 		
 		
 
+
 	}
 
-	public void onBtnCancelarAction() {
+	public void onBtnCancelarAction() {	
 		Telas tela = new Telas();
 
 		tela.loadView("/gui/TelaInicial.fxml");
@@ -135,23 +108,40 @@ public class TelaCadastrarEntregasController implements Initializable {
 
 		lbSemestreAno.setText(String.format("%d° / %d", semestralizacao, anoAtual));
 
-		// preenchimento do ChoiceBox Turma
+		// preenchimento do combobox TIPO
 
-		List<TurmasDTO> listaTurmas = new ArrayList<TurmasDTO>();
+		List<TipoDTO> listaTipo = new ArrayList<TipoDTO>();
 
 		try {
 
-			listaTurmas = LoadTurmas.carregaTurmas(semestralizacao, anoAtual);
+			// lista tipos
+			listaTipo = LoadTipo.carregaTipos();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			Alerts.showAlert("SQLException", "Erro ao buscar turmas", "Ocorreu um erro ao buscar as turmas para o semestre atual.", AlertType.ERROR);
+			Alerts.showAlert("SQLException", "Erro ao buscar turmas",
+					"Ocorreu um erro ao buscar as turmas para o semestre atual.", AlertType.ERROR);
 		}
+		
 
-		if (listaTurmas != null) {
-			ObservableList<TurmasDTO> turmas = FXCollections.observableArrayList(listaTurmas);
-			choiceBoxTurma.getItems().addAll(turmas);
+		if (listaTipo != null) {
+			ObservableList<TipoDTO> tipos = FXCollections.observableArrayList(listaTipo);
+			choiceBoxTipo.setItems(tipos);
 		}
+		
+		choiceBoxTipo.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue != null) {
+				if (newValue.getTipo().equals("Portifólio")) {
+					check1.setVisible(true);
+					check2.setVisible(true);
+				} else {
+					check1.setVisible(false);
+					check2.setVisible(false);
+				}
+			}
+		});
+
+
 
 	}
 
