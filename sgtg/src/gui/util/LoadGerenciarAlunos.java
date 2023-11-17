070973f50fb81f69c8f6dea305f5bfeea424f46a
Andre-Bernardes200 <127262908+Andre-Bernardes200@users.noreply.github.com>
2023-11-17 20:48:11 -0300

    fix: finalização da visualização de entregas no gerenciar aluno

--- sgtg/src/gui/util/LoadGerenciarAlunos.java
+++ sgtg/src/gui/util/LoadGerenciarAlunos.java
@@ -25,7 +25,7 @@ public class LoadGerenciarAlunos {
 	    
 	    while (result.next()) {
 	    	
-	    	int id = result.getInt("id_aluno");
+	    	int id_aluno = result.getInt("id_aluno");
 	    	String nome_aluno = result.getString("nome_aluno");
 	    	String emailAlunoFatec = result.getString("email_fatec_aluno");
 	    	String emailAlunoPessoal = result.getString("email_pessoal");
@@ -42,26 +42,27 @@ public class LoadGerenciarAlunos {
 	    	int id_orientador = result.getInt("id_orientador");
 	    	
 //	    	System.out.println(id +" - "+ nome_aluno+" - "+ nome_orientador+" - "+ turma+" - "+ tipo+" - "+ id_turma);	
-	    	GerenciarAlunoDTO aluno = new GerenciarAlunoDTO(id,nome_aluno,emailAlunoPessoal,emailAlunoFatec,nome_orientador,emailOrientador,nome_turma,tipo_tg,tituloTg,empresa,disciplina,regra,id_turma,id_orientador);
+	    	GerenciarAlunoDTO aluno = new GerenciarAlunoDTO(id_aluno,nome_aluno,emailAlunoPessoal,emailAlunoFatec,nome_orientador,emailOrientador,nome_turma,tipo_tg,tituloTg,empresa,disciplina,regra,id_turma,id_orientador);
 	    	
 	    	
 	    	// total de entregas
 	    	int total_entregas = 0;
 	    	int entrega_aluno = 0;
 	    	
-	    	PreparedStatement st2 = conecta.prepareStatement("select count(entrega.id) as n_entrega from entrega_tipo inner join entrega on entrega.id = entrega_tipo.id_entrega inner join entrega_turma on entrega.id = entrega_turma.id_entrega where id_tipo = ? and id_turma = ? and entrega.visibility = 1");
+	    	PreparedStatement st2 = conecta.prepareStatement("select count(entrega.id) as n_entrega from entrega_tipo inner join entrega on entrega.id = entrega_tipo.id_entrega inner join entrega_turma on entrega.id = entrega_turma.id_entrega inner join matricula on entrega_turma.id_turma = matricula.id_turma where entrega_tipo.id_tipo = ? and entrega_turma.id_turma = ? and id_aluno = ? and entrega.visibility = 1");
 	    	st2.setInt(1, id_tipo);
 	    	st2.setInt(2, id_turma);
+	    	st2.setInt(3, id_aluno);
 	    	
 	    	ResultSet result2 = st2.executeQuery();
 	    	
 	    	while (result2.next()) {
-	    		total_entregas += result2.getInt("n_entregas");
+	    		total_entregas += result2.getInt("n_entrega");
 			}
 	    	
 	    	// entregas feitas pelo aluno
 	    	PreparedStatement st3 = conecta.prepareStatement("select count(id_aluno) entrega_aluno from feedback where id_aluno = ? and visibility = 1");
-	    	st3.setInt(1, id);
+	    	st3.setInt(1, id_aluno);
 	    	
 	    	ResultSet result3 = st3.executeQuery();
 	    	
@@ -85,7 +86,6 @@ public class LoadGerenciarAlunos {
 	            for (int j = i ; j < listaAlunos.size(); j++) {
 	                if (alunoAtual.getId_aluno() == (listaAlunos.get(j).getId_aluno())&& alunoAtual.getId_turma() !=(listaAlunos.get(j).getId_turma()) ) {
 	                	alunoAtual.setNome_turma("TG1 e TG2");
-	                	alunoAtual.setTotal_entregas(alunoAtual.getTotal_entregas()+listaAlunos.get(j).getTotal_entregas());
 	                    listaAlunos.remove(j);
 	                }
 	                
